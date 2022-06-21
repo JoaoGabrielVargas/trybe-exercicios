adb25cbbe236ad09ebecf70ccd2d880b7dc9c272
JoaoGabrielVargas <joao.gaiares@gmail.com>
2022-06-21 15:35:40 -0300

    primeiro exercicio da explicação do JS

---
+++ bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts.js
@@ -0,0 +1,10 @@
+const myName = "João Gabriel";
+const birthCity = "Rio de Janeiro";
+let birthYear = 1991;
+
+birthYear = 2030;
+
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);

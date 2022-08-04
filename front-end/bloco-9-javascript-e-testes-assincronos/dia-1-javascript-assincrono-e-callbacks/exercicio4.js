1243a7cac2f0dadb4ce7c70095edcb6e808f06c4
JoaoGabrielVargas <joao.gaiares@gmail.com>
2022-08-04 18:16:49 -0300

    exercicio 4

---
+++ front-end/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/exercicio4.js
@@ -0,0 +1,14 @@
+const messageDelay = () => Math.floor(Math.random() * 5000);
+
+const getMarsTemperature = () => {
+  const maxTemperature = 58;
+  return Math.floor(Math.random() * maxTemperature);
+};
+
+// crie a função sendMarsTemperature abaixo
+
+const sendMarsTemperature = () => {
+  console.log(`A temperatura em marte é de ${getMarsTemperature()}`);
+}
+
+setTimeout(() => sendMarsTemperature(), messageDelay());
